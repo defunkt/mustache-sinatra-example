d5e992745ee98a6691120ea1d73d68ec10705750
defunkt <chris@ozmm.org>
2009-10-28 15:49:34 -0700

    edit test

--- app.rb
+++ app.rb
@@ -4,8 +4,8 @@ require 'mustache/sinatra'
 class App < Sinatra::Base
   register Mustache::Sinatra
 
-  set :views,     'templates/'
-  set :mustaches, 'views/'
+  set :views,     "templates/"
+  set :mustaches, "views/"
 
   get '/' do
     @title = "Mustache + Sinatra = Wonder"
